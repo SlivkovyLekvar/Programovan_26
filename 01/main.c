0b2afd269669cdabbbec6cfabe2bf1812d9ee147
Nicol Tomková <tomkonic@fel.cvut.cz>
2026-03-11 11:36:30 +0100

    added vyhrazne spravy

--- 01/main.c
+++ 01/main.c
@@ -1,3 +1,11 @@
+// Tento zdrojovy kod som vypracovala samostatne bez cizi pomoci
+// Nekopirovala som ani neopisovala cudzie zdrojove kody
+// Nikto mi pri vypracovani neradil
+// Ak niektory riadok tieto pravidla porusuje, je oznaceny komentarom
+// TOTO NENI MOJE TVORBA
+// Poruseni pravidel je podvod a tresta sa VZLOUCENIM ZE STUDIA
+// Nicol Tomkova, 43106
+
 #include <stdio.h>
 #include "list.h"
 
