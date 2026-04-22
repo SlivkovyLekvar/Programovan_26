d1dc39a20faa0f1cb953716e8b2ef7c54f1f85a6
Nicol Tomková <tomkonic@fel.cvut.cz>
2026-04-22 16:08:45 +0200

    prepared file for lesson 3

---
+++ 03/main.c
@@ -0,0 +1,11 @@
+// Tento zdrojovy kod som vypracovala samostatne bez cizi pomoci
+// Nekopirovala som ani neopisovala cudzie zdrojove kody
+// Nikto mi pri vypracovani neradil
+// Ak niektory riadok tieto pravidla porusuje, je oznaceny komentarom
+// TOTO NENI MOJE TVORBA
+// Poruseni pravidel je podvod a tresta sa VYLOUCENIM ZE STUDIA
+// Nicol Tomkova, 43106
+
+int main() {
+    return 0;
+}
